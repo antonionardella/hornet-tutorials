c16e3be1f7ede1d420ce309d02068f0014303071
Antonio Nardella <antonio.nardella@iota.org>
2020-06-10 11:20:40 +0200

    quick and dirty hornet on VPS
    quick and dirty hornet installation for tutorials

---
+++ quickhornet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade && \
+wget -qO - https://ppa.hornet.zone/pubkey.txt | sudo apt-key add - && \
+sudo sh -c 'echo "deb http://ppa.hornet.zone stable main" >> /etc/apt/sources.list.d/hornet.list' && \
+sudo apt update && sudo apt install hornet ufw sudo fail2ban && \
+ufw allow outcoming && ufw deny incoming && ufw allow ssh && ufw allow 14626/udp && ufw allow 15600/tcp && \
+adduser beekeeper && usermod -aG sudo beekeeper && \
+sudo systemctl start fail2ban && sudo systemctl enable fail2ban
