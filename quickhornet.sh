147b977b0261175b0f3967fb05dad6aed50d847d
Antonio Nardella <antonio.nardella@iota.org>
2020-06-10 11:26:45 +0200

    Update quickhornet.sh

--- quickhornet.sh
+++ quickhornet.sh
@@ -3,6 +3,6 @@ sudo apt update && sudo apt upgrade --assume-yes && \
 wget -qO - https://ppa.hornet.zone/pubkey.txt | sudo apt-key add - && \
 sudo sh -c 'echo "deb http://ppa.hornet.zone stable main" >> /etc/apt/sources.list.d/hornet.list' && \
 sudo apt update && sudo apt install --assume-yes hornet ufw sudo fail2ban && \
-ufw allow outcoming && ufw deny incoming && ufw allow ssh && ufw allow 14626/udp && ufw allow 15600/tcp && \
+ufw allow outgoing && ufw deny incoming && ufw allow ssh && ufw allow 14626/udp && ufw allow 15600/tcp && \
 adduser beekeeper && usermod -aG sudo beekeeper && \
 sudo systemctl start fail2ban && sudo systemctl enable fail2ban
